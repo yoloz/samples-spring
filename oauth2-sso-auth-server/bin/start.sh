682c6f961edee065247f397e862b1914a0d53f9f
yoloz <youloz@outlook.com>
2021-07-30 11:01:49 +0800

    添加启动脚本

---
+++ oauth2-sso-auth-server/bin/start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+export JAVA_HOME='/opt/jdk1.8.0_162'
+if [ "x$JAVA_HOME" = "x" ]; then
+   echo "JAVA_HOME is not configured, please configure and then execute again!"
+   exit 1
+fi
+path=$(cd `dirname $0`/..;pwd)
+nohup ${JAVA_HOME}/bin/java -jar ${path}/lib/oauth2-sso-auth-server-0.0.1-SNAPSHOT.jar > ${path}/server.log 2>&1 &
---
+++ oauth2-sso-client-usermsg/bin/stop.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+PID=$(ps -ef | grep 'oauth2-sso-client-usermsg-0.0.1-SNAPSHOT.jar' |grep -v grep|awk '{print $2}')
+if [ -z "$PID" ]; then
+    echo "oauth2-sso-client-usermsg-0.0.1-SNAPSHOT not running"
+else
+    echo "stop oauth2-sso-client-usermsg-0.0.1-SNAPSHOT,pid is " ${PID}
+    kill -s TERM ${PID}
+fi
+exit 0
